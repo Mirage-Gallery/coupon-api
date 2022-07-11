da3681870c986517d3d76be2b919940c84817ce3
Lucas Negritto <lucas.negritto@gmail.com>
2022-07-11 10:06:18 +0100

    cors

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 require('dotenv').config();
 const port = process.env.PORT || 3000;
+const cors = require('cors')
+
+app.use(cors())
 
 const Coupon = require('./model.js')
 
