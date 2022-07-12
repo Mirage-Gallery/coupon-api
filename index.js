069227be454287dd6aed9b66e3e1946116655666
Lucas Negritto <lucas.negritto@gmail.com>
2022-07-12 11:43:36 +0100

    log tests

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 const cors = require('cors')
+const logger = require('./logger')
 
 app.use(cors())
 
@@ -12,9 +13,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/coupon/:address', (req, res) => {
-  console.log(res.statusCode)
+app.get('/log', (req, res) => {
+  res.sendFile(__dirname + '/combined.log')
+})
 
+app.get('/coupon/:address', (req, res) => {
+  // log URL
+  logger.info(`GET /coupon/${req.params.address}`)
+  logger.info(res.statusCode)
   const userAddress = req.params.address;
   Coupon.findOne({ address: userAddress }, function (err, coupon) {
     if (err) console.log(err)
@@ -34,6 +40,9 @@ app.get('/loaderio-5c062010c267d966c18fdb3c4a118112', (req, res) => {
 
 // catch all 404
 app.use((req, res, next) => {
+  // log URL
+  logger.info(`${req.method} ${req.url}`)
+  logger.info(res.statusCode)
   res.status(404).send('404: Page not found')
 })
 
